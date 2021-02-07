6886db9b3d7b91a68b562a4c579d4d8788072b6f
Jayson Aviso <jayson.aviso1@gmail.com>
2021-02-07 21:18:26 +0800

    Building the UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponent,
+    LogsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
